4de0a4764e16cd3326246472ca5e5a5c89ba0b76
Cangramorael <heitorcandeira@hotmail.com>
2024-03-02 17:06:07 -0300

    Game Manager Update Score
    In GameManager Script added a score and highest score.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,19 @@ public class GameManager : MonoBehaviour
     // Singleton
     public static GameManager Instance {get; private set;}
 
+    // Constants
+    private static readonly string KEY_HIGHEST_SCORE = "HighestScore";
+
     // API
     public bool isGameOver {get; private set;}
+
+    [Header("Audio")]
     [SerializeField] private AudioSource musicGame;
     [SerializeField] private AudioSource gameOverSfx;
+
+    [Header("Score")]
+    [SerializeField] private float score;
+    [SerializeField] private int highestScore;
 
     void Awake() {
         if(Instance != null && Instance != this) {
@@ -18,6 +27,30 @@ public class GameManager : MonoBehaviour
         } else {
             Instance = this;
         }
+
+        // Score
+        score = 0;
+        highestScore = PlayerPrefs.GetInt(KEY_HIGHEST_SCORE);
+    }
+
+    void Update() {
+        if(!isGameOver) {
+            // Increment score
+            score += Time.deltaTime;
+
+            // Update highest score
+            if(GetScore() > GetHighetScore()) {
+                highestScore = GetScore();
+            }
+        }
+    }
+
+    public int GetScore() {
+        return (int) Mathf.Floor(score);
+    }
+
+    public int GetHighetScore() {
+        return highestScore;
     }
 
     public void EndGame() {
@@ -31,6 +64,9 @@ public class GameManager : MonoBehaviour
 
         //Play SFX
         gameOverSfx.Play();
+
+        // Save highest score
+        PlayerPrefs.SetInt(KEY_HIGHEST_SCORE, GetHighetScore());
     }
 
 }
